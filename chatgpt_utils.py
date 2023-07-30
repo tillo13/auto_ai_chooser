5e536552d0dea296ff3170b0333e6f0156a7a8d9
Andy Tillo <andy.tillo@gmail.com>
2023-07-30 16:48:17 -0700

    use tiktoken to get cleaner resutls

--- chatgpt_utils.py
+++ chatgpt_utils.py
@@ -1,37 +1,62 @@
 import os
 import time
+import tiktoken
 import openai
 
 def get_chatgpt_response(user_input, model_choice, openai_secret_key):
     start_time = time.time()
-    response = get_response_for_user_input(user_input, model_choice, openai_secret_key)
+    full_response = get_response_for_user_input(user_input, model_choice, openai_secret_key)
     duration = time.time() - start_time
-    return response, duration
+    return full_response, duration
 
 def get_response_for_user_input(user_input, incoming_model, openai_secret_key):
     openai.api_key = openai_secret_key
 
     prompt = f"{user_input}"
 
-    if incoming_model == "gpt-3.5-turbo":
-        messages=[
-            {"role": "system", "content":"You are a helpful assistant."},
-            {"role": "user", "content": prompt},
-        ]
+    tokenizer = tiktoken.get_encoding("cl100k_base")  # Use default encoding
+    input_tokens = len(tokenizer.encode(prompt))
 
-        response = openai.ChatCompletion.create(
+    # Define a buffer for the message structure (might need adjustment)
+    message_buffer = 50
+
+    # Define a maximum value for response tokens (might need adjustment)
+    max_response_tokens = 500
+
+    if incoming_model.startswith("gpt-3.5"):
+        if incoming_model.endswith("16k"):
+            model_max_tokens = 16384  # GPT-3.5 Turbo 16K
+        else:
+            model_max_tokens = 4096   # other GPT-3.5 models
+
+    elif incoming_model.startswith("text-davinci"):
+        model_max_tokens = 4097   # Text Davinci models
+
+    elif incoming_model.startswith("code-davinci"):
+        model_max_tokens = 8001   # Code Davinci models
+
+    elif incoming_model.startswith("gpt-4"):
+        if incoming_model.endswith("32k"):
+            model_max_tokens = 32768  # GPT-4 32K models
+        else:
+            model_max_tokens = 8192   # other GPT-4 models
+
+    else:
+        return "Model not recognized.  Contact your local developer."
+
+    messages=[
+        {"role": "system", "content":"You are a helpful assistant."},
+        {"role": "user", "content": prompt},
+    ]
+
+    max_tokens = min(model_max_tokens - input_tokens - message_buffer, max_response_tokens)
+
+    response = openai.ChatCompletion.create(
         model=incoming_model,
         messages=messages,
         temperature=0.6,
-        max_tokens=1000
-        )
-        result = response.choices[0].message['content'].strip()
+        max_tokens=max_tokens
+    )
 
-    elif incoming_model == "text-davinci-003":
-        response = openai.Completion.create(engine=incoming_model, prompt=prompt, max_tokens=1000)
-        result = response.choices[0].text.strip()
-
-    else:
-        result = "Model not recognized.  Contact your local developer."
-
-    return result
+    # Instead of returning a single message, return the entire response
+    return response
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from chatgpt_utils import get_response_for_user_input
 #from secrets_manager import get_secret_version
 from config import SITE_HOMEPAGE,GCP_PROJECT_ID, OPENAI_SECRET_KEY, OPENAI_DEFAULT_MODEL
+import json
 
 app = Flask(__name__)
 
@@ -19,13 +20,12 @@ def result():
     if user_input != 'empty':
         try:
             from chatgpt_utils import get_chatgpt_response
-            gpt_response, chatgpt_time = get_chatgpt_response(user_input, OPENAI_DEFAULT_MODEL, OPENAI_SECRET_KEY)
-            user_input += f"\nChatGPT Response: {gpt_response}"
+            full_gpt_response, chatgpt_time = get_chatgpt_response(user_input, OPENAI_DEFAULT_MODEL, OPENAI_SECRET_KEY)
+            user_input += f"ChatGPT Response: {json.dumps(full_gpt_response, indent=2)}"
         except Exception as e:
             print(f"Failed to get chatGPT response. Error: {e}")
             chatgpt_time = 0
     return f"You typed: {user_input}"
-
 
 if __name__ == "__main__":
     app.run(debug=True)
